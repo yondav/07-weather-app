bc3932de2c85e4cbde3d6d5848e16b75b54212ef
yondav <yonidavid.abr@gmail.com>
2021-03-08 18:19:53 -0500

    hourly weather works and is styled but I need to get the time to increase by an hour for each card...for loop?

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ const currentUV = document.querySelector('.currentUV');
 const currentDesc = document.querySelector('.currentDesc');
 const currentWind = document.querySelector('.currentWind');
 const currentIcon = document.querySelector('.currentIcon');
+const mainHourly = document.querySelector('.hourly-card');
 
 let lat;
 let long;
@@ -26,7 +27,7 @@ window.addEventListener('DOMContentLoaded', function () {
     long = position.coords.longitude.toString();
     console.log(lat, long);
 
-    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${unit}&exclude=daily,hourly,minutely&appid=93fbb945657a5e5ca75650241870b021`;
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${unit}&exclude=daily,minutely&appid=93fbb945657a5e5ca75650241870b021`;
 
     fetch(currentWeatherURL).then(function (response) {
       return response.json().then(function (data) {
@@ -43,6 +44,25 @@ window.addEventListener('DOMContentLoaded', function () {
         currentHumidity.textContent = data.current.humidity + '%';
         currentFeels.textContent = Math.floor(data.current.feels_like) + deg;
         currentUV.textContent = data.current.uvi.toString();
+
+        // console log for 24 hours of the 48 given
+        console.log(data.hourly.slice(0, -24));
+        let hourlyCard = data.hourly.slice(0, -24).map(function (item) {
+          const hourlyTemp = Math.floor(item.temp) + deg;
+          const hourlyIconCode = item.weather[0].icon;
+          const hourlyIconSource = `http://openweathermap.org/img/wn/${hourlyIconCode}@2x.png`;
+          const hourlyDesc = item.weather[0].description;
+
+          return `<div class="hourly-wrap">
+              <div class="hourlyTime-card">
+                <h6 class="hourly">10AM</h6>
+                <img src="${hourlyIconSource}" alt="${hourlyDesc}" class="hourlyIcon">
+                <h4 class="hourlytemp">${hourlyTemp}</h4>
+              </div>
+            </div>`;
+        });
+        hourlyCard = hourlyCard.join('');
+        mainHourly.innerHTML = hourlyCard;
       });
     });
   });
