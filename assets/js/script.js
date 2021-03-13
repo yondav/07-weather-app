a1dba2daf672e0aecf90b722fae0bdb4ea063c03
yondav <yonidavid.abr@gmail.com>
2021-03-13 01:30:58 -0500

    working on local storage bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,171 +64,127 @@ window.addEventListener('DOMContentLoaded', function () {
     savedCarrot.classList.add('hide');
     savedCont.classList.add('hide');
   }
-});
 
-// get weather from search bar - use mapbox for query
-searchForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const value = locationInput.value;
-
-  fetch(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${value}.json?access_token=pk.eyJ1IjoieW9uZGF2IiwiYSI6ImNrbTMwdzVrcDFiOHEyb3FzMmZyM3BraTMifQ.WqR9QQOGPMezPeLCeRrelg`
-  ).then(function (response) {
-    return response.json().then(function (data) {
-      const queryLat = data.features[0].geometry.coordinates[1];
-      const queryLong = data.features[0].geometry.coordinates[0];
-      const dataCity = data.features[0].context[0].text;
-      const dataState = data.features[0].context[1].text;
-      const dataCountry = data.features[0].context[2].text;
-
-      city.textContent = dataCity;
-      state.textContent = dataState;
-      country.textContent = dataCountry;
-      getWeather(queryLat, queryLong);
-
-      // save to local storage
-      let savedValue = `${dataCity}, ${dataState}`;
-      let items = getLocalStorage();
-      items.push(savedValue);
-      localStorage.setItem('location', JSON.stringify(items));
-
-      searchForm.reset();
-    });
-  });
-});
-
-// toggle search bar
-searchIcon.addEventListener('click', function () {
-  inputCont.classList.toggle('show-input');
-  locationInput.focus();
-  // savedCont.classList.add('show-links');
-});
-
-// browse saved searches
-savedCarrot.addEventListener('click', function () {
-  savedCont.classList.toggle('show-links');
-});
-
-// get coordinates
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
-  } else {
-    locationFail();
+  // get coordinates
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
+    } else {
+      locationFail();
+    }
   }
-}
 
-// function for startup if statement - success
-function locationSuccess(position) {
-  lat = position.coords.latitude.toString();
-  long = position.coords.longitude.toString();
-  getWeather(lat, long);
-}
-// function for startup if statement - fail, opens modal
-function locationFail(error) {
-  //show the modal here. You might not really need to use the error param here, makes sense to me to just launch the modal regardless
-  showModal.classList.add('show-modal');
-  searchModal.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let zipCode = modalInput.value;
-    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=93fbb945657a5e5ca75650241870b021`;
+  // function for startup if statement - success
+  function locationSuccess(position) {
+    lat = position.coords.latitude.toString();
+    long = position.coords.longitude.toString();
+    getWeather(lat, long);
+  }
+  // function for startup if statement - fail, opens modal
+  function locationFail(error) {
+    //show the modal here. You might not really need to use the error param here, makes sense to me to just launch the modal regardless
+    showModal.classList.add('show-modal');
+    searchModal.addEventListener('submit', function (e) {
+      e.preventDefault();
+      let zipCode = modalInput.value;
+      const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=93fbb945657a5e5ca75650241870b021`;
+
+      fetch(currentWeatherURL).then(function (response) {
+        return response.json().then(function (data) {
+          getWeather(data.coord.lat, data.coord.lon);
+          searchForm.reset();
+          showModal.classList.remove('show-modal');
+        });
+      });
+    });
+  }
+
+  // fetch call
+  function getWeather(lat, long) {
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${unit}&exclude=minutely&appid=${weatherKey}`;
 
     fetch(currentWeatherURL).then(function (response) {
       return response.json().then(function (data) {
-        getWeather(data.coord.lat, data.coord.lon);
-        searchForm.reset();
-        showModal.classList.remove('show-modal');
-      });
-    });
-  });
-}
+        const iconCode = data.current.weather[0].icon;
+        const iconSource = `./assets/icons/${iconCode}.svg`;
+        const currentUV = data.current.uvi;
+        const latData = data.lat;
+        const lonData = data.lon;
 
-// fetch call
-function getWeather(lat, long) {
-  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${unit}&exclude=minutely&appid=${weatherKey}`;
+        currentIcon.src = iconSource;
+        currentIcon.alt = data.current.weather[0].description;
+        currentTemp.textContent = Math.floor(data.current.temp) + deg;
+        currentDesc.textContent = data.current.weather[0].description;
+        currentWind.textContent = Math.floor(data.current.wind_speed) + 'mph';
+        currentHumidity.textContent = data.current.humidity + '%';
+        currentFeels.textContent = Math.floor(data.current.feels_like) + deg;
+        uvIndex.textContent = currentUV.toString();
 
-  fetch(currentWeatherURL).then(function (response) {
-    return response.json().then(function (data) {
-      const iconCode = data.current.weather[0].icon;
-      const iconSource = `./assets/icons/${iconCode}.svg`;
-      const currentUV = data.current.uvi;
-      const latData = data.lat;
-      const lonData = data.lon;
+        // uv index color coding
+        if (currentUV < 3) {
+          uvIcon.innerHTML = `<i class="fas fa-chevron-down"></i>`;
+          uvIcon.style.color = '#9dfb98';
+          uvKey.textContent = 'low';
+          uvKey.style.color = '#9dfb98';
+        } else if (currentUV > 3 && currentUV < 6) {
+          uvIcon.innerHTML = `<i class="fas fa-window-minimize"></i>`;
+          uvIcon.style.color = '#6aced5';
+          uvKey.textContent = 'moderate';
+          uvKey.style.color = '#6aced5';
+        } else if (currentUV > 6 && currentUV < 8) {
+          uvIcon.innerHTML = `<i class="fas fa-chevron-up"></i>`;
+          uvIcon.style.color = '#d56a6a';
+          uvKey.textContent = 'high';
+          uvKey.style.color = '#d56a6a';
+        } else if (currentUV > 8 && currentUV < 11) {
+          uvIcon.innerHTML = `<i class="fas fa-chevron-up"></i><i class="fas fa-chevron-up"></i>`;
+          uvIcon.style.color = '#b64747';
+          uvKey.textContent = ' very high';
+          uvKey.style.color = '#b64747';
+        } else if (currentUV > 11) {
+          uvIcon.innerHTML = `<i class="fas fa-exclamation-triangle"></i>`;
+          uvIcon.style.color = '#cf3333';
+          uvKey.textContent = ' danger';
+          uvKey.style.color = '#cf3333';
+        }
 
-      currentIcon.src = iconSource;
-      currentIcon.alt = data.current.weather[0].description;
-      currentTemp.textContent = Math.floor(data.current.temp) + deg;
-      currentDesc.textContent = data.current.weather[0].description;
-      currentWind.textContent = Math.floor(data.current.wind_speed) + 'mph';
-      currentHumidity.textContent = data.current.humidity + '%';
-      currentFeels.textContent = Math.floor(data.current.feels_like) + deg;
-      uvIndex.textContent = currentUV.toString();
+        // hourly get specs and append
+        let hourlyCard = data.hourly.slice(0, -24).map(function (item) {
+          const hourlyTemp = Math.floor(item.temp) + deg;
+          const hourlyIconCode = item.weather[0].icon;
+          const hourlyIconSource = `./assets/icons/${hourlyIconCode}.svg`;
+          const hourlyDesc = item.weather[0].description;
 
-      // uv index color coding
-      if (currentUV < 3) {
-        uvIcon.innerHTML = `<i class="fas fa-chevron-down"></i>`;
-        uvIcon.style.color = '#9dfb98';
-        uvKey.textContent = 'low';
-        uvKey.style.color = '#9dfb98';
-      } else if (currentUV > 3 && currentUV < 6) {
-        uvIcon.innerHTML = `<i class="fas fa-window-minimize"></i>`;
-        uvIcon.style.color = '#6aced5';
-        uvKey.textContent = 'moderate';
-        uvKey.style.color = '#6aced5';
-      } else if (currentUV > 6 && currentUV < 8) {
-        uvIcon.innerHTML = `<i class="fas fa-chevron-up"></i>`;
-        uvIcon.style.color = '#d56a6a';
-        uvKey.textContent = 'high';
-        uvKey.style.color = '#d56a6a';
-      } else if (currentUV > 8 && currentUV < 11) {
-        uvIcon.innerHTML = `<i class="fas fa-chevron-up"></i><i class="fas fa-chevron-up"></i>`;
-        uvIcon.style.color = '#b64747';
-        uvKey.textContent = ' very high';
-        uvKey.style.color = '#b64747';
-      } else if (currentUV > 11) {
-        uvIcon.innerHTML = `<i class="fas fa-exclamation-triangle"></i>`;
-        uvIcon.style.color = '#cf3333';
-        uvKey.textContent = ' danger';
-        uvKey.style.color = '#cf3333';
-      }
+          // get hour for hourly display
+          const hourUnix = item.dt;
+          const hour = moment(hourUnix * 1000).format('ha');
 
-      // hourly get specs and append
-      let hourlyCard = data.hourly.slice(0, -24).map(function (item) {
-        const hourlyTemp = Math.floor(item.temp) + deg;
-        const hourlyIconCode = item.weather[0].icon;
-        const hourlyIconSource = `./assets/icons/${hourlyIconCode}.svg`;
-        const hourlyDesc = item.weather[0].description;
-
-        // get hour for hourly display
-        const hourUnix = item.dt;
-        const hour = moment(hourUnix * 1000).format('ha');
-
-        return `<div class="hourly-wrap">
+          return `<div class="hourly-wrap">
             <div class="hourlyTime-card">
               <h6 class="hourly">${hour}</h6>
               <img src="${hourlyIconSource}" alt="${hourlyDesc}" class="hourlyIcon">
               <h4 class="hourlytemp">${hourlyTemp}</h4>
             </div>
           </div>`;
-      });
-      hourlyCard = hourlyCard.join('');
-      sectionHourly.innerHTML = hourlyCard;
+        });
+        hourlyCard = hourlyCard.join('');
+        sectionHourly.innerHTML = hourlyCard;
 
-      let dailyCard = data.daily.map(function (item) {
-        const dailyTemp = Math.floor(item.temp.day) + deg;
-        const dailyHigh = Math.floor(item.temp.max) + deg;
-        const dailyLow = Math.floor(item.temp.min) + deg;
-        const dailyIconCode = item.weather[0].icon;
-        const dailyIconSource = `./assets/icons/${dailyIconCode}.svg`;
-        const dailyDesc = item.weather[0].description;
-        const dailyHumidity = item.humidity;
-        const dailyWind = item.wind_speed + '%';
+        let dailyCard = data.daily.map(function (item) {
+          const dailyTemp = Math.floor(item.temp.day) + deg;
+          const dailyHigh = Math.floor(item.temp.max) + deg;
+          const dailyLow = Math.floor(item.temp.min) + deg;
+          const dailyIconCode = item.weather[0].icon;
+          const dailyIconSource = `./assets/icons/${dailyIconCode}.svg`;
+          const dailyDesc = item.weather[0].description;
+          const dailyHumidity = item.humidity;
+          const dailyWind = item.wind_speed + '%';
 
-        // get date for day display
-        const dateUnix = item.dt;
-        const date = moment(dateUnix * 1000).format('ddd, MMM DD');
+          // get date for day display
+          const dateUnix = item.dt;
+          const date = moment(dateUnix * 1000).format('ddd, MMM DD');
 
-        return `<div class="daily-card">
+          return `<div class="daily-card">
         <div class="card-center">
           <h5 class="dailyDate">${date}</h5>
           <img src="${dailyIconSource}" alt="${dailyDesc}" class="daily-icon">
@@ -254,89 +210,152 @@ function getWeather(lat, long) {
           </div>
         </div>
       </div>`;
-      });
-      dailyCard = dailyCard.join('');
-      sectionDaily.innerHTML = dailyCard;
-
-      // for location display
-      if (
-        city.textContent == '' &&
-        state.textContent == '' &&
-        country.textContent == ''
-      ) {
-        fetch(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${lonData},${latData}.json?access_token=pk.eyJ1IjoieW9uZGF2IiwiYSI6ImNrbTMwdzVrcDFiOHEyb3FzMmZyM3BraTMifQ.WqR9QQOGPMezPeLCeRrelg`
-        ).then(function (response) {
-          return response.json().then(function (data) {
-            const dataCity = data.features[4].context[0].text;
-            const dataState = data.features[4].context[1].text;
-            const dataCountry = data.features[4].context[2].text;
-            city.innerHTML = `${dataCity},`;
-            state.innerHTML = `${dataState}`;
-            country.innerHTML = `${dataCountry}`;
-          });
         });
-      }
+        dailyCard = dailyCard.join('');
+        sectionDaily.innerHTML = dailyCard;
+
+        // for location display
+        if (
+          city.textContent == '' &&
+          state.textContent == '' &&
+          country.textContent == ''
+        ) {
+          fetch(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${lonData},${latData}.json?access_token=pk.eyJ1IjoieW9uZGF2IiwiYSI6ImNrbTMwdzVrcDFiOHEyb3FzMmZyM3BraTMifQ.WqR9QQOGPMezPeLCeRrelg`
+          ).then(function (response) {
+            return response.json().then(function (data) {
+              const dataCity = data.features[4].context[0].text;
+              const dataState = data.features[4].context[1].text;
+              const dataCountry = data.features[4].context[2].text;
+              city.innerHTML = `${dataCity},`;
+              state.innerHTML = `${dataState}`;
+              country.innerHTML = `${dataCountry}`;
+            });
+          });
+        }
+      });
+    });
+  }
+
+  // get weather from search bar - use mapbox for query
+  searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = locationInput.value;
+
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${value}.json?access_token=pk.eyJ1IjoieW9uZGF2IiwiYSI6ImNrbTMwdzVrcDFiOHEyb3FzMmZyM3BraTMifQ.WqR9QQOGPMezPeLCeRrelg`
+    ).then(function (response) {
+      return response.json().then(function (data) {
+        const queryLat = data.features[0].geometry.coordinates[1];
+        const queryLong = data.features[0].geometry.coordinates[0];
+        const dataCity = data.features[0].context[0].text;
+        const dataState = data.features[0].context[1].text;
+        const dataCountry = data.features[0].context[2].text;
+
+        city.textContent = dataCity;
+        state.textContent = dataState;
+        country.textContent = dataCountry;
+        getWeather(queryLat, queryLong);
+
+        // save to local storage
+        let savedValue = `${dataCity}, ${dataState}`;
+        let items = getLocalStorage();
+        items.push(savedValue);
+        localStorage.setItem('location', JSON.stringify(items));
+
+        let lastKey = getLocalStorage();
+        lastKey = lastKey.length - 1;
+        console.log(lastKey[lastKey.length - 1]);
+        const savedLocation = document.createElement('li');
+        savedLocation.textContent = lastKey;
+        savedSearch.appendChild(savedLocation);
+
+        if (savedCarrot.classList.contains('hide')) {
+          savedCarrot.classList.remove('hide');
+        }
+
+        searchForm.reset();
+      });
     });
   });
-}
 
-// toggle farenheight/celcius and set data-unit attribute
-degToggle.addEventListener('change', function () {
-  if (this.checked) {
-    degMeasurement.setAttribute('data-unit', 'imperial');
-    toggleF.textContent = 'F' + deg;
-    toggleC.textContent = '';
-  } else {
-    degMeasurement.setAttribute('data-unit', 'metric');
-    toggleC.textContent = 'C' + deg;
-    toggleF.textContent = '';
+  // toggle search bar
+  searchIcon.addEventListener('click', function () {
+    inputCont.classList.toggle('show-input');
+    locationInput.focus();
+    // savedCont.classList.add('show-links');
+  });
+
+  // browse saved searches
+  savedCarrot.addEventListener('click', function () {
+    savedCont.classList.toggle('show-links');
+  });
+
+  // toggle farenheight/celcius and set data-unit attribute
+  degToggle.addEventListener('change', function () {
+    if (this.checked) {
+      degMeasurement.setAttribute('data-unit', 'imperial');
+      toggleF.textContent = 'F' + deg;
+      toggleC.textContent = '';
+    } else {
+      degMeasurement.setAttribute('data-unit', 'metric');
+      toggleC.textContent = 'C' + deg;
+      toggleF.textContent = '';
+    }
+    unit = degMeasurement.getAttribute('data-unit');
+    getWeather(lat, long);
+  });
+
+  // background gradient based on time of day
+  function backgroundGradient() {
+    const hour = parseInt(moment().format('H'));
+    if (hour === 6 || hour === 18) {
+      bgNight.classList.toggle('hide');
+      bgSunset.classList.toggle('fadeIn');
+      bgDay.classList.toggle('hide');
+    } else if (hour > 6 && hour < 18) {
+      bgNight.classList.toggle('hide');
+      bgSunset.classList.toggle('hide');
+      bgDay.classList.toggle('fadeIn');
+    } else {
+      bgNight.classList.toggle('fadeIn');
+      bgSunset.classList.toggle('hide');
+      bgDay.classList.toggle('hide');
+    }
   }
-  unit = degMeasurement.getAttribute('data-unit');
-  getWeather(lat, long);
-});
 
-// background gradient based on time of day
-function backgroundGradient() {
-  const hour = parseInt(moment().format('H'));
-  if (hour === 6 || hour === 18) {
-    bgNight.classList.toggle('hide');
-    bgSunset.classList.toggle('fadeIn');
-    bgDay.classList.toggle('hide');
-  } else if (hour > 6 && hour < 18) {
-    bgNight.classList.toggle('hide');
-    bgSunset.classList.toggle('hide');
-    bgDay.classList.toggle('fadeIn');
-  } else {
-    bgNight.classList.toggle('fadeIn');
-    bgSunset.classList.toggle('hide');
-    bgDay.classList.toggle('hide');
+  // local storage
+  function getLocalStorage() {
+    return localStorage.getItem('location')
+      ? JSON.parse(localStorage.getItem('location'))
+      : [];
   }
-}
 
-// local storage
-function getLocalStorage() {
-  return localStorage.getItem('location')
-    ? JSON.parse(localStorage.getItem('location'))
-    : [];
-}
+  // function appendSearchedCity () {
+  //   const lastKey = window.localStorage.key(0)
+  //   const savedLocation = document.createElement('li');
+  //   // return localStorage.getItem(lastKey)
+  //   savedLocation.textContent = lastKey
+  //   savedSearch.appendChild(savedLocation)
+  // }
 
-function appendLocalStorage() {
-  let items = getLocalStorage();
-  for (let i = 0; i < items.length; i++) {
-    const savedLocation = document.createElement('li');
-    savedLocation.addEventListener('click', function () {
-      locationInput.value = items[i];
-      savedCont.classList.remove('show-links');
-      locationInput.focus();
-      savedCarrot.classList.remove('hide');
-      clearBtn.classList.remove('hide');
-    });
-    savedLocation.textContent = items[i];
-    savedSearch.appendChild(savedLocation);
+  function appendLocalStorage() {
+    let items = getLocalStorage();
+    for (let i = 0; i < items.length; i++) {
+      const savedLocation = document.createElement('li');
+      savedLocation.addEventListener('click', function () {
+        locationInput.value = items[i];
+        savedCont.classList.remove('show-links');
+        locationInput.focus();
+        savedCarrot.classList.remove('hide');
+        clearBtn.classList.remove('hide');
+      });
+      savedLocation.textContent = items[i];
+      savedSearch.appendChild(savedLocation);
+    }
   }
-}
 
-clearBtn.addEventListener('click', function () {
-  localStorage.clear();
+  clearBtn.addEventListener('click', function () {
+    localStorage.clear();
+  });
 });
